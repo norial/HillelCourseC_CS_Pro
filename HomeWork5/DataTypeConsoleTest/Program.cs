6880a89bc1a6d7795c15cbe0c37a2e4cd043de58
Olejjas <mollstam4455@gmail.com>
2023-11-28 16:03:11 +0200

    Adding testing

---
+++ HomeWork5/DataTypeConsoleTest/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructuresTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestQueue();
+            TestStack();
+            TestSinglyLinkedList();
+            TestBinarySearchTree();
+            TestLinkedList();
+
+            Console.ReadLine();
+        }
+
+        static void TestQueue()
+        {
+            Console.WriteLine("Testing Queue:");
+
+            Data_Structures_lib.ArrayQueue queue = new Data_Structures_lib.ArrayQueue();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Console.WriteLine($"Count: {queue.Count}");
+
+            Console.WriteLine("Dequeue: " + queue.Dequeue());
+            Console.WriteLine("Peek: " + queue.Peek());
+
+            Console.WriteLine($"Count after Dequeue and Peek: {queue.Count}");
+
+            Console.WriteLine("Clearing the queue...");
+            queue.Clear();
+            Console.WriteLine($"Count after Clear: {queue.Count}");
+
+            Console.WriteLine();
+        }
+
+        static void TestStack()
+        {
+            Console.WriteLine("Testing Stack:");
+
+            Data_Structures_lib.ArrayStack stack = new Data_Structures_lib.ArrayStack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine($"Count: {stack.Count}");
+
+            Console.WriteLine("Pop: " + stack.Pop());
+            Console.WriteLine("Peek: " + stack.Peek());
+
+            Console.WriteLine($"Count after Pop and Peek: {stack.Count}");
+
+            Console.WriteLine("Clearing the stack...");
+            stack.Clear();
+            Console.WriteLine($"Count after Clear: {stack.Count}");
+
+            Console.WriteLine();
+        }
+
+        static void TestSinglyLinkedList()
+        {
+            Console.WriteLine("Testing Singly Linked List:");
+
+            Data_Structures_lib.SinglyLinkedList singlyLinkedList = new Data_Structures_lib.SinglyLinkedList();
+
+            singlyLinkedList.Add(1);
+            singlyLinkedList.Add(2);
+            singlyLinkedList.Add(3);
+
+            Console.WriteLine($"Count: {singlyLinkedList.Count}");
+
+            Console.WriteLine("Clearing the list...");
+            singlyLinkedList.Clear();
+            Console.WriteLine($"Count after Clear: {singlyLinkedList.Count}");
+
+            Console.WriteLine();
+        }
+
+        static void TestBinarySearchTree()
+        {
+            Console.WriteLine("Testing Binary Search Tree:");
+
+            Data_Structures_lib.BinarySearchTree binarySearchTree = new Data_Structures_lib.BinarySearchTree();
+
+            binarySearchTree.Add(2);
+            binarySearchTree.Add(1);
+            binarySearchTree.Add(3);
+
+            Console.WriteLine($"Count: {binarySearchTree.Count}");
+            Console.WriteLine("Contains 2: " + binarySearchTree.Contains(2));
+            Console.WriteLine("Contains 4: " + binarySearchTree.Contains(4));
+
+            Console.WriteLine("Clearing the tree...");
+            binarySearchTree.Clear();
+            Console.WriteLine($"Count after Clear: {binarySearchTree.Count}");
+
+            Console.WriteLine();
+        }
+
+        static void TestLinkedList()
+        {
+            Console.WriteLine("Testing Linked List:");
+
+            Data_Structures_lib.List_On_Array linkedList = new Data_Structures_lib.List_On_Array();
+
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+
+            Console.WriteLine($"Count: {linkedList.Count}");
+            Console.WriteLine("Contains 2: " + linkedList.Contains(2));
+            Console.WriteLine("Contains 4: " + linkedList.Contains(4));
+
+            Console.WriteLine("Clearing the list...");
+            linkedList.Clear();
+            Console.WriteLine($"Count after Clear: {linkedList.Count}");
+
+            Console.WriteLine();
+        }
+    }
+}
