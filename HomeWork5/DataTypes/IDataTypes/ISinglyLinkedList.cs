73d41cab4df848664db2ef194d9979056d9903da
Olejjas <mollstam4455@gmail.com>
2023-12-09 03:26:58 +0200

    Finished DataTypes

---
+++ HomeWork5/DataTypes/IDataTypes/ISinglyLinkedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CollectionInterfaces
+{
+    public interface ISinglyLinkedList<T> 
+    {
+        void AddFirst(T item);
+        void Insert(int index, T item);
+        T FirstValue { get; }
+        T LastValue { get; }
+    }
+}
---
+++ HomeWork5/DataTypes/IDataTypes/IStack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionInterfaces
+{
+    public interface IStack<T> : ICollection<T>
+    {
+        void Push(T item);
+        T Pop();
+        T Peek();
+    }
+}
